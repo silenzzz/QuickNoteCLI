16b3e139777da68e4717ea40ce292dd5279f1b88
Vadim Guseynov <hgt166782@gmail.com>
2021-10-09 12:11:36 +0300

    WIP. Change pom

--- src/main/java/com/demmage/qnc/format/NoteOutputFormatter.java
+++ src/main/java/com/demmage/qnc/format/NoteOutputFormatter.java
@@ -12,16 +12,16 @@ public class NoteOutputFormatter {
 
     public void printList(List<Note> notes) {
 
-        String[][] data = new String[notes.size()][4];
+        final String[][] data = new String[notes.size()][4];
 
         for (int i = 0; i < notes.size(); i++) {
             Note note = notes.get(i);
             int subContentEnd = Math.min(note.getContent().length(), 12);
 
-            data[i][0] = note.getHash().substring(0, 5);
-            data[i][1] = note.getName();
-            data[i][2] = note.getCreated().toString();
-            data[i][3] = (note.getContent().substring(0, subContentEnd)).replace("\n", "");
+            data[i][0] = note.getHash().substring(0, 5) + " ";
+            data[i][1] = note.getName() + " ";
+            data[i][2] = note.getCreated().toString() + " ";
+            data[i][3] = (note.getContent().substring(0, subContentEnd)).replace("\n", "") + " ";
         }
 
         TextTable table = new TextTable(LIST_HEADERS, data);
@@ -33,9 +33,9 @@ public class NoteOutputFormatter {
     public void printNote(Note note) {
         final String[][] data = new String[1][3];
 
-        data[0][0] = note.getHash();
-        data[0][1] = note.getName();
-        data[0][2] = note.getCreated().toString();
+        data[0][0] = note.getHash().substring(0, 5) + " ";
+        data[0][1] = note.getName() + " ";
+        data[0][2] = note.getCreated().toString() + " ";
 
         new TextTable(SINGLE_HEADERS, data).printTable();
 
